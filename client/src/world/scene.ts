b389d50fa110ecb48020ae65994b68f1973f0b30
SHIYI <3401187804@qq.com>
2026-09-22 23:36:57 +0800

    修上一笔的类型笔误: sun.shadow.near→sun.shadow.camera.near

--- client/src/world/scene.ts
+++ client/src/world/scene.ts
@@ -75,7 +75,7 @@ export function createWorld(container: HTMLElement): World {
   sun.shadow.camera.right = 62;
   sun.shadow.camera.top = 62;
   sun.shadow.camera.bottom = -62;
-  sun.shadow.near = 1;
+  sun.shadow.camera.near = 1;
   sun.shadow.camera.far = 200;
   sun.shadow.bias = -0.0004;
   sun.shadow.normalBias = 0.02;
